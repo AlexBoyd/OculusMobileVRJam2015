847c7c46cde395835cadf9be025e7742132588b8
AlexBoyd <alexander.ray.boyd@gmail.com>
2015-04-19 19:27:47 -0700

    initial version of a slide controller. Just swapping main texture of a plane. Using some usued shots I had on my harddrive from some lighting tests we did for a film project.

---
+++ Unity/speakVR/Assets/Scripts/SlideController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SlideController : MonoBehaviour
+{
+
+	public List<Texture> Slides = new List<Texture> ();
+
+	private int mSlideIndex = 0;
+	// Use this for initialization
+	void Start ()
+	{
+		if (Slides != null && Slides.Count > 0) {
+			renderer.material.mainTexture = Slides [0];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			if (Slides.Count > mSlideIndex) {
+				mSlideIndex++;
+				renderer.material.mainTexture = Slides [mSlideIndex];
+
+			}
+		}
+	}
+}
